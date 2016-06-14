d2e6a6bf2a44fb63dbcb3e87eb1a45886cc6a2fd
Jared Yamauchi <jared.yamauchi@gmail.com>
2016-06-14 18:29:52 -0400

    Added FullPage.js

--- alohayamauchi-nonrails/javascripts/main.js
+++ alohayamauchi-nonrails/javascripts/main.js
@@ -1,5 +1,63 @@
 $(document).ready(function () {
 
+  $('#fullpage').fullpage({
+    //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['firstPage', 'secondPage'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: true,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor : ['#ccc', '#fff'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+  });
   //For Navigation Menu
   var nav = $('header');
   var navLi = $('nav li');
@@ -32,6 +90,13 @@ $(document).ready(function () {
       });
       isFixed = false;
     }
+  });
+
+  //
+  $('#contact-me').localScroll({
+    target: '#contact',
+    axis: 'xy',
+    queue: true
   });
 
   // init controller
@@ -76,5 +141,22 @@ $(document).ready(function () {
   //   slidesToShow: 1,
   //   slidesToScroll: 1
   // });
+
+  //Horizontal Scrolling Effect
+  $(function() {
+  $('ul.nav a').bind('click',function(event){
+    var $anchor = $(this);
+    /*
+    if you want to use one of the easing effects:
+    $('html, body').stop().animate({
+      scrollLeft: $($anchor.attr('href')).offset().left
+    }, 1500,'easeInOutExpo');
+     */
+    $('html, body').stop().animate({
+      scrollLeft: $($anchor.attr('href')).offset().left
+    }, 1000);
+    event.preventDefault();
+  });
+});
   
 });
