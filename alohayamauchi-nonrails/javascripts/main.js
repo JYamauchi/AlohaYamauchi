d6d51f1443dfeab9beedead7689c2e243f3b1611
Jared Yamauchi <jared.yamauchi@gmail.com>
2017-04-06 21:00:52 -1000

    Worked on my splash slide

--- alohayamauchi-nonrails/javascripts/main.js
+++ alohayamauchi-nonrails/javascripts/main.js
@@ -49,7 +49,8 @@ $(document).ready(function () {
   var scrollOptions = {click: false,  wheelStep: 20};
   if (Modernizr.touch) {
     scrollOptions.click = true;
-}
+  }
+
   //FullPage JS produces a slide section for each full screen part
   $('#fullpage').fullpage({
     //Accessibility
@@ -58,6 +59,72 @@ $(document).ready(function () {
     scrollOverflowOptions: scrollOptions,
     continuousVertical: true
   });
+
+  $(".jy-ufo-div")
+    .velocity("fadeIn", { duration: 3500 });
+
+  $(".jy-ufo-path")
+    .delay(500)
+    .velocity({ strokeRed: 0, strokeGreen: 0, strokeBlue: 0, opacity: 1, strokeWidth: 2 })
+    // .velocity({ fillRed: 0, fillGreen: 255, fillBlue: 0, opacity: 1})
+    .delay(1000)
+    .velocity({ strokeRed: 255, strokeGreen: 255, strokeBlue: 255, opacity: 1, strokeWidth: 10 })
+    .velocity({ fillRed: 255, fillGreen: 255, fillBlue: 255, opacity: 1});
+
+    $(".jy-ufo-spin-path")
+    .delay(1000)
+    .velocity({ scale: 0.9, fillRed: 0, fillGreen: 0, fillBlue: 0, opacity: 0})
+    .delay(1000)
+    .velocity({ scale: 1, fillRed: 255, fillGreen: 255, fillBlue: 255, opacity: 1});
+
+  var logoSVG = $("#jy-logo-svg")[0];
+  var ufoSVG = $("#jy-ufo-svg")[0];
+
+  createAnimation(logoSVG);
+
+  function createAnimation(element) {
+    var isPlaying = false;
+    var ufoSpin = $(".jy-ufo-spin-path")[0];
+    var ufoSpinScale = TweenMax.to(ufoSpin, 1, {
+      transformOrigin: "50% 50%",
+      scale: 0.98,
+      paused: true
+    }).timeScale(0);
+
+    var rotateCD = TweenMax.to(ufoSVG, 0.3, {
+      rotation: 360, 
+      ease: Power0.easeNone,
+      repeat: -1,
+      paused: true
+    })
+    .timeScale(0);
+      
+    element.addEventListener("mouseover", toggleOn);
+    element.addEventListener("mouseout", toggleOff)
+    
+    function toggleOn() {      
+        isPlaying = true;
+        rotateCD.play();
+        TweenMax.to(rotateCD, 2, { timeScale: 1 });
+        ufoSpinScale.play();
+        TweenMax.to(ufoSpinScale, 2, { timeScale: 1});
+    }
+
+    function toggleOff() {
+      isPlaying = false;            
+      TweenMax.to(rotateCD, 2, { 
+        timeScale: 0, 
+        onComplete: rotateCD.pause, 
+        callbackScope: rotateCD 
+      });
+      TweenMax.to(ufoSpinScale, 2, {
+        scale: 0.9
+        //timeScale: 1,
+        // onComplete: ufoSpinScale.pause,
+        //callbackScope: ufoSpinScale
+      })
+    }
+  }
   
   var button = document.createElement("Button");
     button.innerHTML = "Title";
